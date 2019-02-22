3eebf79dac3374e11906e4e32df51ab05d28c98d
bobrzzz bobrzzz <bobrz-z-z@mail.ru>
2019-02-22 08:13:26 +0100

    new page

--- server.js
+++ server.js
@@ -50,6 +50,14 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        message: 'projects kek'
+    });
+})
+
+
 app.get('/bad', (req, res) => {
     res.send({
         error: 'kek no workl'
